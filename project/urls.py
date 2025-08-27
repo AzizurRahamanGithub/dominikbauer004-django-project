d3f313c6a2946f56d79c4f706e3098b96b731ac0
Azizur Rahaman <your-email@example.com>
2025-08-28 05:45:18 +0600

    add some code

--- project/urls.py
+++ project/urls.py
@@ -29,6 +29,4 @@ urlpatterns = [
     path('api/v1/inquiry/', include('apps.inquiry.urls')),
 ]
 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
